bd3ca4b1f620dc4cdae2b3a5eaad90394c4b1cf1
d.lelashvili <d.lelashvili@just-ai.com>
2024-01-29 18:53:36 +0300

    KS-1-4 fix after review

--- src/main/kotlin/lesson_1/lesson_1_task_4_v1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_4_v1.kt
@@ -2,11 +2,11 @@ package lesson_1
 
 fun main() {
 
-    var length: Long = 40868600000
-    val age: Int = 27
+    val length: Long = 40868600000
+    val age: Byte = 27
     val partOfDay: Float = 0.075f
-    val durationInSeconds: Int = 6480
-    val partOfYear: Double = partOfDay.toDouble() / 365
+    val durationInSeconds: Short = 6480
+    val partOfYear: Double = 2.0547946021981435E-4
     val apogee: Int = 327000
     println(
         """
