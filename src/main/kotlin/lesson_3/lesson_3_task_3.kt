6e23fab4a9ac9bdc8f02dfd6dd96d3a4c217b72f
d.lelashvili <d.lelashvili@just-ai.com>
2024-01-29 23:42:23 +0300

    KS-3-3 multiplication table

--- src/main/kotlin/lesson_3/lesson_3_task_3.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -2,16 +2,10 @@ package lesson_3
 
 fun main() {
 
-    var moveNumber: Byte = 1
-    var from = "E2"
-    var to = "E4"
+    val number: Byte = 5
 
-    println("$from-$to;$moveNumber")
-
-    val d = "D"
-    val fromD: Byte = 2
-    val toD = (fromD + 1).toString()
-
-    println("$d$fromD-$d$toD;${moveNumber + 2}")
+    for (i in 1..9) {
+        println("$number x $i = ${number * i}")
+    }
 
 }
