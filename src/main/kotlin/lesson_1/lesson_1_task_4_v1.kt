9c7ab4c2cb18662a7885b7cb65105fa509cfab7b
d.lelashvili <d.lelashvili@just-ai.com>
2024-01-29 18:52:03 +0300

    Revert "KS-2-4 calculate and print buff"
    This reverts commit 837eb6f47b6308c7a090b7ae67b1ebb5eab86cf8.

--- src/main/kotlin/lesson_1/lesson_1_task_4_v1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_4_v1.kt
@@ -2,11 +2,11 @@ package lesson_1
 
 fun main() {
 
-    val length: Long = 40868600000
-    val age: Byte = 27
+    var length: Long = 40868600000
+    val age: Int = 27
     val partOfDay: Float = 0.075f
-    val durationInSeconds: Short = 6480
-    val partOfYear: Double = 2.0547946021981435E-4
+    val durationInSeconds: Int = 6480
+    val partOfYear: Double = partOfDay.toDouble() / 365
     val apogee: Int = 327000
     println(
         """
