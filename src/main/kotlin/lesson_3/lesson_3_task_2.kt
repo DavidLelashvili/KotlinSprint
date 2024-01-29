5688592faf9fa5542a218602a219ad2a50c576b8
d.lelashvili <d.lelashvili@just-ai.com>
2024-01-29 22:48:32 +0300

    KS-3-2 change second name

---
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -0,0 +1,16 @@
+package lesson_3
+
+fun main() {
+
+    val name = "Татьяна"
+    var secondName = "Андреева"
+    val middleName = "Сергеевна"
+    var age = 20
+
+    println("$secondName $name $middleName, $age")
+
+    secondName = "Сидорова"
+    age = 22
+
+    println("$secondName $name $middleName, $age")
+}
