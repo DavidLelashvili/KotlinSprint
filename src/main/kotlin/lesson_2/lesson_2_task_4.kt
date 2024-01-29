9c0b778063481d21510b93e266bb20514c2555c8
d.lelashvili <d.lelashvili@just-ai.com>
2024-01-29 15:46:23 +0300

    KS-2-4 calculate and print buff

---
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -0,0 +1,14 @@
+package lesson_2
+
+fun main() {
+
+    val iron = 11
+    val crystal = 7
+    val buff = 20
+
+    val ironBuff = iron * buff / 100
+    val crystalBuff = crystal * buff / 100
+
+    println("Бафф железной руды - $ironBuff")
+    println("Бафф кристаллической руды - $crystalBuff")
+}
