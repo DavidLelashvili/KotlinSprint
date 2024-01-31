6f743f70ab842be4fe52721f8b8d749fee0f9fae
DavidLelashvili <87592352+DavidLelashvili@users.noreply.github.com>
2024-02-01 00:03:34 +0300

    KS-1-4 print data about flight (#6)
    * KS-1-4 print data about flight
    
    * KS-2-4 calculate and print buff
    
    * Revert "KS-2-4 calculate and print buff"
    
    This reverts commit 837eb6f47b6308c7a090b7ae67b1ebb5eab86cf8.
    
    * KS-1-4 fix after review
    
    * KS-1-4 review fixes. delete v2
    
    ---------
    
    Co-authored-by: d.lelashvili <d.lelashvili@just-ai.com>

---
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -0,0 +1,23 @@
+package lesson_1
+
+fun main() {
+
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val durationInSeconds: Short = 6480
+    val partOfYear: Double = 2.0547946021981435E-4
+    val apogee: Int = 327000
+    println(
+        """
+        Расстояние - $length
+        Возраст - $age
+        Часть дня - $partOfDay
+        Секунды - $durationInSeconds
+        Часть года - $partOfYear
+        Апогей арбиты - $apogee
+    """.trimIndent()
+    )
+
+}
+
