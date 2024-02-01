1f228b2201835d762f6e5e265cefa9f727a9e53d
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-01 22:51:59 +0300

    KS-2-4 review fixes

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -1,13 +1,15 @@
 package lesson_2
 
+const val PERCENT_100 = 100
+
 fun main() {
 
     val iron: Byte = 11
     val crystal: Byte = 7
     val buff: Byte = 20
 
-    val ironBuff = iron * buff / 100
-    val crystalBuff = crystal * buff / 100
+    val ironBuff = iron * buff / PERCENT_100
+    val crystalBuff = crystal * buff / PERCENT_100
 
     println("Бафф железной руды - $ironBuff")
     println("Бафф кристаллической руды - $crystalBuff")
