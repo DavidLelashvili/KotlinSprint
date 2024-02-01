54562be5fef52e7128025b58d0ad142413888410
DavidLelashvili <87592352+DavidLelashvili@users.noreply.github.com>
2024-02-01 22:44:42 +0300

    KS-2-5 calculate interest (#9)
    * KS-2-5 calculate interest
    
    * KS-2-5 change type
    
    ---------
    
    Co-authored-by: d.lelashvili <d.lelashvili@just-ai.com>

---
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -0,0 +1,20 @@
+package lesson_2
+
+import kotlin.math.pow
+
+fun main() {
+
+//    Формула: S=A*(1+R)^T
+//    A- СУММА ВКЛАДА;
+//    R- СТАВКА ПРОЦЕНТА;
+//    T- КОЛИЧЕСТВО ПЕРИОДОВ;
+//    S- ПОЛУЧАЕМАЯ СУММА.
+
+    val deposit = 70000
+    val interestRate: Double = 16.7
+    val time = 20
+    val s = deposit * (1 + interestRate / 100).pow(time)
+
+    println((String.format("%.3f", s)))
+
+}
