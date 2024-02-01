7256e1cc39266d863a78f5e8cfbeaf8faa4712eb
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-01 23:03:24 +0300

    KS-3-3 remove for

--- src/main/kotlin/lesson_3/lesson_3_task_3.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -4,8 +4,28 @@ fun main() {
 
     val number: Byte = 5
 
-    for (i in 1..9) {
-        println("$number x $i = ${number * i}")
-    }
+    val i1 = 1
+    val i2 = 2
+    val i3 = 3
+    val i4 = 4
+    val i5 = 5
+    val i6 = 6
+    val i7 = 7
+    val i8 = 8
+    val i9 = 9
+
+    println(
+        """
+        $number X $i1 = ${number * i1}
+        $number X $i2 = ${number * i2}
+        $number X $i3 = ${number * i3}
+        $number X $i4 = ${number * i4}
+        $number X $i5 = ${number * i5}
+        $number X $i6 = ${number * i6}
+        $number X $i7 = ${number * i7}
+        $number X $i8 = ${number * i8}
+        $number X $i9 = ${number * i9}
+    """.trimIndent()
+    )
 
 }
