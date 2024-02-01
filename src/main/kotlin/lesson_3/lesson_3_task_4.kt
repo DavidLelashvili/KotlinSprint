1e19fd6973ec91edc712b7785fb2d17b91a6c646
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-01 23:13:26 +0300

    KS-3-4 add const val and move number

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -1,17 +1,20 @@
 package lesson_3
 
+const val MOVE_TO_ONE_CELL = 1
+
 fun main() {
 
-    val moveNumber: Byte = 1
+    val moveNumberWhite1: Byte = 1
+    val moveNumberWhite2: Byte = 3
     val from = "E2"
     val to = "E4"
 
-    println("$from-$to;$moveNumber")
+    println("$from-$to;$moveNumberWhite1")
 
     val d = "D"
     val fromD: Byte = 2
-    val toD = (fromD + 1).toString()
+    val toD = (fromD + MOVE_TO_ONE_CELL).toString()
 
-    println("$d$fromD-$d$toD;${moveNumber + 2}")
+    println("$d$fromD-$d$toD;${moveNumberWhite2}")
 
 }
