2c7446fad0e399953dc2c94f4b8caf7b7fddb004
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-02 10:23:20 +0300

    KS-4-1 add const val to check free tables

--- src/main/kotlin/lesson_4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -1,14 +1,15 @@
 package lesson_4
 
 const val tables: Byte = 13
+const val CHECK_FREE_TABLES: Byte = 0
 
 fun main() {
 
     val reservedTablesToday: Byte = 13
     val reservedTablesTomorrow: Byte = 9
 
-    val freeTablesToday: Boolean = (tables - reservedTablesToday) > 0
-    val freeTablesTomorrow: Boolean = (tables - reservedTablesTomorrow) > 0
+    val freeTablesToday: Boolean = (tables - reservedTablesToday) > CHECK_FREE_TABLES
+    val freeTablesTomorrow: Boolean = (tables - reservedTablesTomorrow) > CHECK_FREE_TABLES
 
     println("Доступных столиков на сегодня: $freeTablesToday\nДоступных столиков на завтра: $freeTablesTomorrow")
 }
