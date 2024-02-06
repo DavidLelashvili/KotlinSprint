54133677ff88eacd393099aa5b267a20e32f5708
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-06 10:47:13 +0300

    KS-6-4 review fixes

--- src/main/kotlin/lesson_6/lesson_6_task_4.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_4.kt
@@ -9,7 +9,11 @@ fun main() {
 
     val number = Random.nextInt(1..9)
     var userNumber: Int
+    var rightNumber: Boolean
     var i = 0
+
+
+    println("У вас есть $TRIES попыток, чтобы угадать число от 1 до 9!")
 
     do {
         if (i == TRIES) {
@@ -17,13 +21,15 @@ fun main() {
             break
         }
 
-        if (i == 0) println("У вас есть $TRIES попыток, чтобы угадать число от 1 до 9!\nВведите число")
-        else println("Неудача, попробуйте снова\nВведите число")
+        println("Введите число")
         userNumber = readln().toInt()
 
-        if (userNumber == number) println("Вы угадали! Это была великолепная игра!")
+        rightNumber = (userNumber == number)
+
+        if (rightNumber) println("Вы угадали! Это была великолепная игра!")
+        else println("Неудача, попробуйте снова")
 
         i++
-    } while (userNumber != number)
+    } while (!rightNumber)
 
 }
