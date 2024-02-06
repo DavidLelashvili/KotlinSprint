787418dfd44076501182861f4e1e7eb6ee967522
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-07 00:22:59 +0300

    KS-7-1 generate random pass

--- src/main/kotlin/lesson_7/lesson_7_task_1.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_1.kt
@@ -2,4 +2,15 @@ package lesson_7
 
 fun main() {
 
+    val numbersRange = 0..9
+    val alphabetRange = 'a'..'z'
+    val passwordLenght = 6
+    var pass = ""
+
+    for (i in 1..passwordLenght / 2) {
+        pass += alphabetRange.random().toString() + numbersRange.random().toString()
+    }
+
+    println(pass)
+
 }
