57656373faebc7b4db9cfe1ad7b043d38847e611
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-07 23:33:39 +0300

    KS-7-2 send and check verify code

---
+++ src/main/kotlin/lesson_7/lesson_7_task_2.kt
@@ -0,0 +1,23 @@
+package lesson_7
+
+fun main() {
+
+    val verifyCodeRange = (1000..9999)
+    var verifyCode: Int
+    var userCode : Int
+    var rightCode: Boolean
+
+    do {
+        verifyCode = verifyCodeRange.random()
+        println("Ваш код авторизации: $verifyCode")
+
+        println("Введите код")
+        userCode = readln().toInt()
+
+        rightCode = userCode==verifyCode
+
+    } while (!rightCode)
+
+    println("Добро подаловать!")
+
+}
