9f9a09e01282ec5ff7699bede6d8a8341c90987c
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-11 00:04:33 +0300

    KS-8-4 remove for

--- src/main/kotlin/lesson_8/lesson_8_task_4.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_4.kt
@@ -3,8 +3,8 @@ package lesson_8
 fun main() {
 
     val ingredientsArray = arrayOf("яйцо", "молоко", "салат", "помидор", "огурец", "лаваш", "сыр", "соль")
-    var ingredientContains = false
     val userNewIngredient: String
+    val indexReplaceIngredient :Int
 
     println("Вот какие ингредиенты входят сейчас в рецепт:")
     ingredientsArray.forEach { print("$it ") }
@@ -12,20 +12,13 @@ fun main() {
     println("Введите ингредиент, которых хотите изменить")
     val userReplaceIngredient = readln()
 
-    for (i in ingredientsArray) {
-        ingredientContains = userReplaceIngredient == i
-        if (!ingredientContains) {
-            continue
-        } else {
-            println("Ингредиент найден, на какой хотите заменить")
-            userNewIngredient = readln()
+    if (userReplaceIngredient in ingredientsArray) {
+        println("Ингредиент найден, на какой хотите заменить")
+        userNewIngredient = readln()
 
-            ingredientsArray.set(ingredientsArray.indexOf(i), userNewIngredient)
-            break
-        }
-    }
+        indexReplaceIngredient = ingredientsArray.indexOf(userReplaceIngredient)
+        ingredientsArray.set(indexReplaceIngredient, userNewIngredient)
 
-    if (ingredientContains) {
         println("Готово! Вы сохранили следующий список:")
         ingredientsArray.forEach { print("$it ") }
     } else println("Нет такого ингредиента")
