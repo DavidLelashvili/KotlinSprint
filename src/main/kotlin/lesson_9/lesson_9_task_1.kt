3a089b389fe4166266d74cc3be658ab033b310b1
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-11 23:39:39 +0300

    KS-9-1 print ingredients in recipe

--- src/main/kotlin/lesson_9/lesson_9_task_1.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_1.kt
@@ -2,4 +2,9 @@ package lesson_9
 
 fun main() {
 
+    val ingredietnsList = listOf("яйцо", "молоко", "салат", "помидор", "огурец", "лаваш", "сыр", "соль")
+
+    println("В рецепте есть следующие ингредиенты:")
+    ingredietnsList.forEach { println(it) }
+
 }
