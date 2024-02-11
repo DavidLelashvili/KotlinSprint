97fa1f340a70cd2d9141f6472f355fd8f34332de
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-11 23:29:47 +0300

    KS-8-4 review fixes

--- src/main/kotlin/lesson_8/lesson_8_task_4.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_4.kt
@@ -4,20 +4,22 @@ fun main() {
 
     val ingredientsArray = arrayOf("яйцо", "молоко", "салат", "помидор", "огурец", "лаваш", "сыр", "соль")
     val userNewIngredient: String
-    val indexReplaceIngredient :Int
+    val indexReplaceIngredient: Int
 
     println("Вот какие ингредиенты входят сейчас в рецепт: ${ingredientsArray.contentToString()}")
     println("Введите ингредиент, которых хотите изменить")
     val userReplaceIngredient = readln()
 
-    if (ingredientsArray.contains(userReplaceIngredient)) {
+    indexReplaceIngredient = ingredientsArray.indexOf(userReplaceIngredient)
+
+    if (indexReplaceIngredient == -1) println("Нет такого ингредиента")
+    else {
         println("Ингредиент найден! Введите ингредиент, на который хотите заменить")
         userNewIngredient = readln()
 
-        indexReplaceIngredient = ingredientsArray.indexOf(userReplaceIngredient)
         ingredientsArray.set(indexReplaceIngredient, userNewIngredient)
 
         println("Готово! Вы сохранили следующий список: ${ingredientsArray.contentToString()}")
-    } else println("Нет такого ингредиента")
+    }
 
 }
