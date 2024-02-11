85ac828ce23e7063edca90eb0cded93190c3f7eb
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-11 23:41:26 +0300

    KS-9-1 rename it

--- src/main/kotlin/lesson_9/lesson_9_task_1.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_1.kt
@@ -5,6 +5,8 @@ fun main() {
     val ingredietnsList = listOf("яйцо", "молоко", "салат", "помидор", "огурец", "лаваш", "сыр", "соль")
 
     println("В рецепте есть следующие ингредиенты:")
-    ingredietnsList.forEach { println(it) }
+    ingredietnsList.forEach { ingredient ->
+        println(ingredient)
+    }
 
 }
