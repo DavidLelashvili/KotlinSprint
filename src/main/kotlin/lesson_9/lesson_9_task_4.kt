fe1b2741bdb3dd370eb5ea587cfdd0c8f7fe4f4a
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-12 21:38:05 +0300

    KS-9-4 return sorted list of ingredients

---
+++ src/main/kotlin/lesson_9/lesson_9_task_4.kt
@@ -0,0 +1,9 @@
+package lesson_9
+
+fun main() {
+
+    println("Введите 5 ингредиентов для вашего блюда через ;")
+    val ingredientsList = readln().split(";")
+
+    println("${ingredientsList.sorted()}")
+}
