0f08b11cd2f3c94f93807d5113bf8c4a8781e98d
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-14 10:47:36 +0300

    KS-9-4 review fixes

--- src/main/kotlin/lesson_9/lesson_9_task_4.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_4.kt
@@ -2,8 +2,8 @@ package lesson_9
 
 fun main() {
 
-    println("Введите 5 ингредиентов для вашего блюда через ;")
-    val ingredientsList = readln().replace(" ", "").split(";")
+    println("Введите 5 ингредиентов для вашего блюда через ,")
+    val ingredientsList = readln().split(", ")
 
     println("${ingredientsList.sorted()}")
 }
