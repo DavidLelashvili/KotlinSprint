25c88b5f024ef7cd58b73d0e538351c957cfe901
d.lelashvili <d.lelashvili@just-ai.com>
2024-02-21 22:29:14 +0300

    KS-11-2 print user info after add bio and change pass

---
+++ src/main/kotlin/lesson_11/lesson_11_task_2.kt
@@ -0,0 +1,52 @@
+package lesson_11
+
+class User2(
+    val id: Int,
+    val login: String,
+    var pass: String,
+    val mail: String,
+    var bio: String? = null,
+) {
+    fun getUserInfo() {
+        println("""
+            Ваши данные:
+            id - $id
+            login - $login
+            password - $pass
+            mail - $mail
+            bio - $bio
+        """.trimIndent())
+    }
+
+    fun getUserBio() {
+        println("Введите информацию о себе")
+        bio = readln()
+    }
+
+    fun changeUserPassword() {
+        println("Для смены пароля сначала введите свой текущий пароль")
+        val oldPass = readln()
+
+        if (oldPass == pass) {
+            println("Отлично! Введите новый пароль")
+            pass = readln()
+            println("Ваш пароль успешно изменен!")
+        }
+    }
+
+}
+
+fun main() {
+
+    val user = User2(
+        getRandomId(),
+        "login1",
+        "pass1",
+        "mail@mail.com",
+    )
+
+    user.getUserBio()
+    user.changeUserPassword()
+    user.getUserInfo()
+
+}
